da17931e3ae2dd3b70779d5925c1e5ca191a1a91
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-04-05 09:31:25 +0600

    create single link component

---
+++ src/components/SingleLink.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function SingleLink({ to, Icon, title, menuIsOpen }) {
+    return (
+        <NavLink
+            title={title}
+            className={`flex ${!menuIsOpen && 'justify-center'} items-center text-md py-2 px-5  w-full text-gray-700 font-medium`}
+            style={({ isActive }) => {
+                return {
+                    color: isActive ? '#fff' : 'rgb(55 65 81)',
+                    background: isActive ? "#0D53FC" : "transparent",
+                    borderBottom: isActive ? '5px solid #0044E9' : '5px solid transparent'
+                };
+            }}
+            to={to} >
+            <Icon
+             className={`${menuIsOpen ? 'text-md mr-3' : 'text-xl mr-0'}`} />
+            {menuIsOpen && title}
+        </NavLink>
+    )
+}
